5390ff94b709ed19c61e141413d91b83949050a1
Jorge Fuentes <jsjfuentesj@gmail.com>
2024-09-22 03:24:38 -0700

    fix types

--- app/Main.tsx
+++ app/Main.tsx
@@ -102,11 +102,11 @@ export default async function Main({ posts }) {
           })}
         </ul>
       </div>
-      {siteMetadata.newsletter?.provider && (
+      {/* {siteMetadata.newsletter?.provider && (
         <div className="flex items-center justify-center pt-4">
           <NewsletterForm />
         </div>
-      )}
+      )} */}
     </>
   )
 }
